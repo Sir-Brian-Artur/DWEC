b89b061d4bc1deaf92d8299ccead7e40d65eeeda
Sir-Brian-Artur <arturfarobrian@gmail.com>
2025-03-18 07:11:27 +0100

    Update view.js

--- Brian Artur/view.js
+++ Brian Artur/view.js
@@ -5,18 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
     let users = JSON.parse(localStorage.getItem("users")) || [];
 
     function renderUsers() {
-        userGrid.innerHTML = "";            
+        userGrid.innerHTML = "";           //Limpiar padre 
         users.forEach((user, index) => {                
-            const card = document.createElement("div");
-            card.className = "card";
+            const card = document.createElement("div");     //1user-1div
+            card.className = "card";            //class a cada div
+
             card.innerHTML = `
-          <p><strong>Nombre:</strong> ${user.nombre}</p>
-          <p><strong>Apellidos:</strong> ${user.apellidos}</p>
-          <p><strong>Email:</strong> ${user.email}</p>
-          <p><strong>Contraseña:</strong> ${user.contrasena}</p>
-          <p><strong>Edad:</strong> ${user.edad}</p>
-          <button data-index="${index}">Eliminar</button>
-        `;
+                <p><strong>Nombre:</strong> ${user.nombre}</p>
+                <p><strong>Apellidos:</strong> ${user.apellidos}</p>
+                <p><strong>Email:</strong> ${user.email}</p>
+                <p><strong>Contraseña:</strong> ${user.contrasena}</p>
+                <p><strong>Edad:</strong> ${user.edad}</p>
+                <button data-index="${index}">Eliminar</button>
+            `;
             userGrid.appendChild(card);
         });
     }
